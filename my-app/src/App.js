0562b9f46b4b1fec73442c9b4dac083e81522c71
uabj8000 <uabj8000@gmail.com>
2021-02-16 19:27:47 -0300

    BrowserRouter as Router, Switch, Route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { AddUser } from "./components/AddUser";
+import { EditUser } from "./components/EditUser";
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/add" component={AddUser} />
+          <Route path="/edit/:id" component={EditUser} />
+        </Switch>
+      </Router>
     </div>
   );
 }
