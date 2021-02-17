4427c32117ce3a35fa3bfbcf9124a92c11bfce8f
uabj8000 <uabj8000@gmail.com>
2021-02-16 22:49:58 -0300

    fix indentation

--- my-app/src/components/AddUser.js
+++ my-app/src/components/AddUser.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 export const AddUser = () => {
-    return (
-        <div>
-            <h1>Add</h1>
-            <Form>
-            <FormGroup>
-                <Label>Name</Label>
-                <Input type="text" placeholder="Enter Name"></Input>
-            </FormGroup>
-            <Button type="submit">Submit</Button>
-            <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
-            </Form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Add</h1>
+      <Form>
+        <FormGroup>
+          <Label>Name</Label>
+          <Input type="text" placeholder="Enter Name"></Input>
+        </FormGroup>
+        <Button type="submit">Submit</Button>
+        <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
+      </Form>
+    </div>
+  )
 }
