181e95c8555fd63bc47a26ab5d35781f3bc10541
uabj8000 <uabj8000@gmail.com>
2021-02-16 21:55:50 -0300

    add forms

--- my-app/src/components/EditUser.js
+++ my-app/src/components/EditUser.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 export const EditUser = () => {
     return (
         <div>
-            <h1>Edit User</h1>
+            <h1>Edit</h1>
+            <Form>
+            <FormGroup>
+                <Label>Name</Label>
+                <Input type="text" placeholder="Enter Name"></Input>
+            </FormGroup>
+            <Button type="submit">Submit</Button>
+            <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
+            </Form>
         </div>
     )
 }
